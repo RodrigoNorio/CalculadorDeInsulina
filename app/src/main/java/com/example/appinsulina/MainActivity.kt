31b9687862fde7be4752f89537457b0f9e2e25be
unknown <norio.rnt@gmail.com>
2023-09-18 17:24:48 -0300

    add edit text and button

--- app/src/main/java/com/example/appinsulina/MainActivity.kt
+++ app/src/main/java/com/example/appinsulina/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.appinsulina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var carbohydrate: EditText
+  private lateinit var weight: EditText
+  lateinit var btnCalculate: Button
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    setupView()
+    setupListeners()
+  }
+
+  fun setupView() {
+    carbohydrate = findViewById(R.id.input_grams)
+    weight = findViewById(R.id.input_weight)
+    btnCalculate = findViewById(R.id.btn_calculate_insulina)
+  }
+
+  fun setupListeners() {
+    btnCalculate.setOnClickListener{
+      val inputCarbohydrate = carbohydrate.text.toString()
+      val inputWeight = weight.text.toString()
+      Toast.makeText(applicationContext, inputCarbohydrate, Toast.LENGTH_SHORT).show()
+    }
   }
 }
